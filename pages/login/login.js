78ec81e7948f2ff76d44e60126d4a8cf2ecb7798
guoruize <guoruize@360.cn>
2022-08-11 11:29:03 +0800

    feat：登录页面

--- pages/login/login.js
+++ pages/login/login.js
@@ -2,65 +2,31 @@
 Page({
 
   /**
-   * 页面的初始数据
+   * 微信登录
    */
-  data: {
-
+  wechatLogin: function() {
+    console.log('wechatLogin');
   },
 
   /**
-   * 生命周期函数--监听页面加载
+   * 豆瓣帐号登录
    */
-  onLoad(options) {
-
+  doubanLogin: function() {
+    console.log('doubanLogin');
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  openAgreement: function() {
+    console.log('openAgreement');
+    wx.navigateTo({
+      url: '/pages/agreement/agreement',
+      success: result => {
+        
+      },
+      fail: () => {},
+      complete: () => {
+        console.log('jump complete');
+      }
+    });
+      
   }
 })
