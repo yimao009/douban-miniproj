ada299bd25386907508ce054667011b843d9115c
guoruize <guoruize@360.cn>
2022-08-14 03:11:02 +0800

    feat: 个人界面完成布局

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -5,7 +5,44 @@ Page({
    * 页面的初始数据
    */
   data: {
+    items: [
+      {
+        icon: 'ic_cat_movie.png',
+        title: '观影分析',
+        count: 0,
+        has: '看过',
+        mark: '标记10部影片\n开启观影分析',
+      },
+      {
+        icon: 'ic_cat_book.png',
+        title: '读书分析',
+        count: 0,
+        has: '读过',
+        mark: '标记10本书\n开启读书分析',
+      },
+      {
+        icon: 'ic_cat_music.png',
+        title: '音乐分析',
+        count: 0,
+        has: '听过',
+        mark: '标记10张唱片\n开启音乐分析',
+      }
+    ]
+  },
 
+  begin: function(evt) {
+    // evt.currentTarget.id == "item-0"
+    const idx = evt.currentTarget.dataset.index;
+    if(idx == 0) {
+      console.log("观影分析");
+    } else if (idx == 1) {
+      console.log("读书分析");
+    } else if (idx == 2) {
+      console.log("音乐分析");
+    }
+    // console.log(evt.currentTarget.dataset.index);
+    // console.log(ent);
+    
   },
 
   /**
