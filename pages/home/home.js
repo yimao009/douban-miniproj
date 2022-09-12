3c8f3006040e8ce89877329450fa391bef8cbb0c
guoruize <guoruize@360.cn>
2022-09-12 18:25:47 +0800

    feat：首页 影院热映 横向列表开发完成

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,9 +1,30 @@
 // pages/home/home.js
 Page({
+  data: {
+    movies: []
+  },
 
   onLoad: function() {      
-    this.loadCity((city) => {
-      console.log('获取城市信息', city);
+    // this.loadCity(this.loadData);
+  },
+  loadData: function(city) {
+    wx.request({
+      url: 'url',
+      data: { city: city},
+      header: {'content-type': 'json'},
+      success: (res) => {
+        console.log(res);
+        let movies = res.data.subjects;
+        // this.data.movies = res.data.subjects;
+        // this.setData(this.data);
+        for(let index = 0; index < movies.length; index++) {
+          this.updateMovie(array[index]);
+        }
+        this.setData({ movies : movies });
+      },
+      fail: () => {
+        wx.db.toastError('获取热映失败');
+      }
     });
   },
 
@@ -17,23 +38,35 @@ Page({
             ak:"AS4pY6soYVDlGwpznang1VOGvjphcamz",
             output:"json",
             coordtype:"wgs84ll",
-            location:result.latitude + "," + result.longitude
+            location: '${result.latitude},${result.longitude}'
           },
           success: (res) => {
             let city = res.data.result.addressComponent.city;
             city = city.substring(0,city.length -1);
-            console.log(city);
-            success(city);
+            success && success(city);
           },
           fail: () => {
             wx.db.toastSuccess('获取城市错误');
           },
         });
-          
       },
       fail: () => {
         wx.db.toastSuccess('获取位置失败');
       }
     });
+  },
+
+  updateMovie:function(movie) {
+    let stars = parseInt(movie.rating.stars);
+    if(stars ==0) return;
+    movie.stars = [1,1,1,0.5,0];
+
+
+    // 另一种方法
+    // movie.stars = {};
+    // movie.stars.on = parseInt(stars / 10);
+    // movie.stars.half = (stars - (movie.stars.on) * 10) > 0;
+    // movie.stars.off = parseInt((50 - stars) / 10);
   }
+
 })
